cec9493e57b84cbf1406d7bdde0d4dea00bae62c
niki <niki.dimov86@gmail.com>
2021-05-22 21:26:36 +0300

    exercise

---
+++ Basic Syntax, Conditional Statements and Loops/christmas_spirit.py
@@ -0,0 +1,27 @@
+quantity = int(input())
+days_left = int(input())
+total_cost = 0
+total_spirit = 0
+for day in range(1, days_left + 1):
+    if day % 11 == 0:
+        quantity += 2
+    if day % 2 == 0:
+        total_cost += 2 * quantity
+        total_spirit += 5
+    if day % 3 == 0:
+        total_cost += 5 * quantity + 3 * quantity
+        total_spirit += 13
+    if day % 5 == 0:
+        total_cost += 15 * quantity
+        total_spirit += 17
+        if day % 3 == 0:
+            total_spirit += 30
+    if day % 10 == 0:
+        total_spirit -= 20
+        total_cost += 5 + 3 + 15
+
+if days_left % 10 == 0:
+    total_spirit -= 30
+print(f"Total cost: {total_cost}\nTotal spirit: {total_spirit}")
+
+
