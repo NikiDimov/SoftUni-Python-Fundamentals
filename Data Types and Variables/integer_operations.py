4f77ff98a98ef732700ff408a6a32be5e7a5368e
niki <niki.dimov86@gmail.com>
2021-05-27 23:04:08 +0300

    exercise

---
+++ Data Types and Variables/integer_operations.py
@@ -0,0 +1,10 @@
+def operation(first_num, second_num, third_num, fourth_num):
+    return int((first_num + second_num) / third_num) * fourth_num
+
+
+first_num = int(input())
+second_num = int(input())
+third_num = int(input())
+fourth_num = int(input())
+
+print(operation(first_num, second_num, third_num, fourth_num))
