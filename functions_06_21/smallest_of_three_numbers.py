7935eb519d83ad4134368ab33a93a506b4df0f82
niki <niki.dimov86@gmail.com>
2021-06-12 22:19:05 +0300

    exercise

---
+++ functions_06_21/smallest_of_three_numbers.py
@@ -0,0 +1,9 @@
+def get_smallest(first, second, third):
+    return min(first, second, third)
+
+
+first_num = int(input())
+second_num = int(input())
+third_num = int(input())
+
+print(get_smallest(first_num, second_num, third_num))
